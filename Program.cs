4e4865d239551b3781dd44db4bb191444fe1808f
Roshan Kumar Singh <roshan_kumar001@yahoo.co.in>
2016-03-17 01:21:52 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace MyFirstGithubApplication
             Console.WriteLine("Extending the master branch.");
             Console.WriteLine("Adding content from rks-hs001.");
             Console.WriteLine("Trying out things on develop branch.");
+            Console.WriteLine("Trying out further things on develop branch.");
             Console.ReadLine();
         }
     }
